caff1a99286a0f786a37ce327ca5628433303c00
SoxMax <irishsoxmax@gmail.com>
2022-01-11 12:50:31 -0500

    window appears and doesn't error

--- scripts/build_effect.lua
+++ scripts/build_effect.lua
@@ -1,8 +1,9 @@
 
 local function buildEffect(sCommand, sParams)
-    
+    Interface.openWindow("effect_builder", "")
 end
 
 function onInit()
     Comm.registerSlashHandler("buildeffect", buildEffect)
+    Comm.registerSlashHandler("effectbuilder", buildEffect)
 end
