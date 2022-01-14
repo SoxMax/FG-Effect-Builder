d903fa8f3e0427e00f1f06142e4a722d164dfff0
SoxMax <irishsoxmax@gmail.com>
2022-01-14 17:35:14 -0500

    change how effect strings are passed up

--- common/scripts/valueholder_effect_editor.lua
+++ common/scripts/valueholder_effect_editor.lua
@@ -1,7 +1,10 @@
 local function updateEffectString(effectString)
-    window.parentcontrol.window.updateEffectString(effectString)
+    window.parentcontrol.updateEffectString(effectString)
 end
 
 function onValueChanged()
+    if super and super.onValueChanged then
+        super.onValueChanged()
+    end
     updateEffectString(window.createEffectString())
 end
