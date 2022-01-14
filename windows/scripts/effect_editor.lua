472db87f7973b664d6bb4ddb1634e295ab0a447f
SoxMax <irishsoxmax@gmail.com>
2022-01-14 09:49:39 -0500

    move effect editor inline with the builder

--- windows/scripts/effect_editor.lua
+++ windows/scripts/effect_editor.lua
@@ -1,7 +1,11 @@
+function delete()
+    close()
+end
+
 function updateEffectType()
     effect_type_editor.setValue(effect_type.getStringValue())
 end
 
 function updateEffectString(effectString)
-    parentcontrol.window.effectDescription.setValue(effectString)
+    effect_description.setValue(effectString)
 end
