e91f0e6178a0fb8fdfab44242e54174c8764f5fe
SoxMax <irishsoxmax@gmail.com>
2022-01-15 18:04:02 -0500

    update healing to support dice

--- editors/35E-PFRPG/scripts/editor_heal.lua
+++ editors/35E-PFRPG/scripts/editor_heal.lua
@@ -1,7 +1,3 @@
 function createEffectString()
-    local effectString = effect.getStringValue() .. ": " .. effect_modifier.getValue()
-    if not effect_bonus_type.isEmpty() then
-        effectString = effectString .. " " .. effect_bonus_type.getValue()
-    end
-    return effectString
+    return effect.getStringValue() .. ": " .. StringManager.convertDiceToString(effect_dice.getDice(), effect_modifier.getValue())
 end
