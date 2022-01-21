18c2a5375a8486eb206f2fdd24bf73c08701ed4a
SoxMax <irishsoxmax@gmail.com>
2022-01-21 09:58:50 -0500

    remove unused templates

--- common/scripts/cycler_effect_subwindow.lua
+++
@@ -1,8 +0,0 @@
-function updateEffectEditor()
-    window.effect_editor.setValue(EditorManager.getEffect(window.effect_category.getStringValue(), getStringValue()).windowclass)
-end
-
-function onValueChanged()
-    updateEffectEditor()
-    super.onValueChanged()
-end
