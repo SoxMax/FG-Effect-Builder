a0f1a064ce0f0683c60a9951cf15ec5efd722348
SoxMax <irishsoxmax@gmail.com>
2022-01-23 22:12:07 -0500

    testing subwindow

--- common/scripts/subwindow_dice_value_editor.lua
+++ common/scripts/subwindow_dice_value_editor.lua
@@ -1,3 +1,4 @@
 function getStringValue()
+    -- Debug.chat("Dice calculation", StringManager.convertDiceToString(effect_dice.getDice(), effect_modifier.getValue()))
     return StringManager.convertDiceToString(effect_dice.getDice(), effect_modifier.getValue())
 end
