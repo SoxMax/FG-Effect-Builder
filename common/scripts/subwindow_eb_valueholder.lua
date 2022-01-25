3584701fa3815bc56ec4a6c9e36ca142ba42a143
SoxMax <irishsoxmax@gmail.com>
2022-01-25 10:51:19 -0500

    dynamically set size based on child window

--- common/scripts/subwindow_eb_valueholder.lua
+++ common/scripts/subwindow_eb_valueholder.lua
@@ -5,3 +5,12 @@ end
 function updateEffectString()
     window.parentcontrol.updateEffectString()
 end
+
+function onInstanceCreated()
+    setWidth()
+end
+
+function setWidth()
+    local width = subwindow.getSize()
+    setAnchoredWidth(width)
+end
