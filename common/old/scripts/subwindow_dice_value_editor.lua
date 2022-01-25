f3dfbb0b97a44b6ed266ffc5205f73bbd80edf2d
SoxMax <irishsoxmax@gmail.com>
2022-01-25 17:15:52 -0500

    update templates for dice number

--- common/old/scripts/subwindow_dice_value_editor.lua
+++ common/old/scripts/subwindow_dice_value_editor.lua
@@ -1,4 +1,3 @@
 function getStringValue()
-    -- Debug.chat("Dice calculation", StringManager.convertDiceToString(effect_dice.getDice(), effect_modifier.getValue()))
     return StringManager.convertDiceToString(effect_dice.getDice(), effect_modifier.getValue())
 end
