1f0cbc780ce6f8ad8db463aa3ab0d0e15eb31525
SoxMax <irishsoxmax@gmail.com>
2022-01-26 10:08:07 -0500

    update dice value function

--- common/scripts/subwindow_eb_dice_value.lua
+++ common/scripts/subwindow_eb_dice_value.lua
@@ -1,12 +1,3 @@
 function getStringValue()
-    local dice = effectDice and effect_dice.getDice()
-    local valueString = StringManager.convertDiceToString(dice, effect_modifier.getValue())
-    local stat = stat_value.getStringValue()
-    if stat ~= "" then
-        if valueString ~= "" then
-            valueString = valueString .. " "
-        end
-        valueString = valueString .. "[" .. stat .."]"
-    end
-    return valueString
+    return StringManager.convertDiceToString(effect_dice.getDice(), effect_modifier.getValue())
 end
