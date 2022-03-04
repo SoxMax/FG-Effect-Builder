0403c60ea2e840fb4b0a6573630b72b306bce82c
SoxMax <irishsoxmax@gmail.com>
2022-03-04 16:51:25 -0500

    update to use DiceManager

--- common/scripts/subwindow_eb_dice_value.lua
+++ common/scripts/subwindow_eb_dice_value.lua
@@ -1,3 +1,3 @@
 function getStringValue()
-    return StringManager.convertDiceToString(effect_dice.getDice(), effect_modifier.getValue())
+    return DiceManager.convertDiceToString(effect_dice.getDice(), effect_modifier.getValue())
 end
