a0e7d3823390edfc2a3050eec36e7f7ab842a9ca
SoxMax <irishsoxmax@gmail.com>
2022-03-04 16:36:13 -0500

    remove old code

--- common/old/scripts/control_valueholder.lua
+++
@@ -1,6 +0,0 @@
-function onValueChanged()
-    if super and super.onValueChanged then
-        super.onValueChanged()
-    end
-    window.parentcontrol.updateEffectString()
-end
--- common/old/scripts/subwindow_valueholder.lua
+++
@@ -1,7 +0,0 @@
-function getStringValue()
-    return subwindow.getStringValue()
-end
-
-function updateEffectString()
-    window.parentcontrol.updateEffectString()
-end
