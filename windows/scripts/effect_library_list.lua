9467b55ff133090ac50b5834bfd1111063488e35
SoxMax <irishsoxmax@gmail.com>
2022-11-20 21:53:07 -0500

    basic display of effects

---
+++ windows/scripts/effect_library_list.lua
@@ -0,0 +1,15 @@
+-- 
+-- Please see the license.html file included with this distribution for 
+-- attribution and copyright information.
+--
+
+function onInit()
+    for _,category in pairs(EditorManager.getCategories()) do
+        for key, editor in pairs(EditorManager.getCategoryEffects(category)) do
+            -- Debug.chat(category, key, editor)
+            local w = createWindow()
+            w.category.setValue(Interface.getString(category))
+            w.name.setValue(editor.label)
+        end
+    end
+end
