145fca7874121ebdce5357dbb40298b90fa135ac
SoxMax <irishsoxmax@gmail.com>
2023-04-16 13:40:29 -0400

    add support for custom lights

--- editors/light/scripts/LIGHT.lua
+++ editors/light/scripts/LIGHT.lua
@@ -1,6 +1,11 @@
 function createEffectString()
     local effectString = "LIGHT: " .. number_value.getStringValue()
-    if not light_type.isEmpty() then
+    if light_type.isEmpty() then
+        effectString = effectString .. " " .. color.getValue()
+        if not light_fx.isEmpty() then
+            effectString = effectString .. " " .. light_fx.getValue() .. " " .. frequency.getValue()
+        end
+    else
         effectString = effectString .. " " .. light_type.getValue()
     end
     return effectString
