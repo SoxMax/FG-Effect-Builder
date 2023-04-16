afa36ac3149b87185b64c84b5cec4d312b138c3a
SoxMax <irishsoxmax@gmail.com>
2023-04-16 14:27:06 -0400

    Light effects (#13)
    * initial light effects
    
    * fix spelling
    
    * update widths
    
    * use controls
    
    * add support for custom lights
    
    * use control
    
    * toggle controls based on selections
    
    * trailing newline

---
+++ editors/light/scripts/LIGHT.lua
@@ -0,0 +1,28 @@
+function createEffectString()
+    local effectString = "LIGHT: " .. number_value.getStringValue()
+    if light_type.isEmpty() then
+        effectString = effectString .. " " .. color.getValue()
+        if not light_fx.isEmpty() then
+            effectString = effectString .. " " .. light_fx.getValue() .. " " .. frequency.getValue()
+        end
+    else
+        effectString = effectString .. " " .. light_type.getValue()
+    end
+    return effectString
+end
+
+function updateVisibility()
+    local isCustomLight = light_type.isEmpty()
+    local hasFx = not light_fx.isEmpty()
+
+    color.setVisible(isCustomLight)
+    light_fx.setComboBoxVisible(isCustomLight)
+    frequency.setVisible(isCustomLight and hasFx)
+end
+
+function onInit()
+    if super and super.onInit then
+        super.onInit()
+    end
+    updateVisibility()
+end
