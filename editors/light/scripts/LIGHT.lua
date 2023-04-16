3d99829b7a480f749081d734d4adad9fc88b4512
SoxMax <irishsoxmax@gmail.com>
2023-04-16 14:13:13 -0400

    toggle controls based on selections

--- editors/light/scripts/LIGHT.lua
+++ editors/light/scripts/LIGHT.lua
@@ -10,3 +10,19 @@ function createEffectString()
     end
     return effectString
 end
+
+function updateVisibility()
+    local isCustomLight = light_type.isEmpty()
+    local hasFx = not light_fx.isEmpty()
+
+    color.setVisible(isCustomLight)
+    light_fx.setComboBoxVisible(isCustomLight)
+    frequency.setVisible(isCustomLight and hasFx)
+end
+
+function onInit()
+    if super and super.onInit then
+        super.onInit()
+    end
+    updateVisibility()
+end
---
+++ editors/light/scripts/LIGHT_values.lua
@@ -0,0 +1,6 @@
+function onValueChanged()
+    window.updateVisibility()
+    if super and super.onValueChanged then
+        super.onValueChanged()
+    end
+end
